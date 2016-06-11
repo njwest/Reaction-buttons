53ad0f7e78a52022cca41eb42c0b93cd7a8906d5
Nick West <mail@nickwe.st>
2016-06-10 20:40:14 -0400

    Styles added, need to fix still image-clicked image gif animate

--- giphy-buttons.js
+++ giphy-buttons.js
@@ -1,12 +1,32 @@
 window.onload = function(){
 
-var reactionArray = ["surprised", "excited", "sad", "scared", "mad", "winning", "lolwut", "omg", "disappointed"];
+$('#addReaction').on('click', function(){
+  reactionArray.push(reactionInput.value);
+  renderButtons();
+  giphyClick();
+  return false;
+});
+
+  $('.clicky').on('click', function(){
+    var state = $(this).attr('data-state');
+    
+    if (state === 'still'){
+      $(this).attr('src', $(this).data('animate'));
+      $(this).attr('data-state', 'animate');
+    }else{
+      $(this).attr('src', $(this).data('still'));
+      $(this).attr('data-state', 'still');
+    }
+  });
+
+
+var reactionArray = ["surprised", "excited", "sad", "scared", "mad", "winning", "lolwut", "omg", "disappointed", "crying", "weird", "yelling", "wtf", "wat", "laugh", "giggle", "chortle", "disgust"];
 
 function renderButtons(){
   $('#buttons').empty();
 
   for (var i = 0; i < reactionArray.length; i++){
-    $('#buttons').append('<button class="giphy" data-reaction="' + reactionArray[i] + '">' + reactionArray[i] + '</button');
+    $('#buttons').append('<button class="giphy" data-state="still" data-reaction="' + reactionArray[i] + '">' + reactionArray[i] + '</button');
   }
 };
 
@@ -29,27 +49,23 @@ function giphyClick(){
         var p = $('<p>').text("Rating: " + rating);
 
         var reactionImage = $('<img>');
+        reactionImage.attr('class', 'clicky');
+        reactionImage.attr('data-state', 'still');
         reactionImage.attr('src', results[i].images.fixed_height.url);
+        reactionImage.attr('data-still', results[i].images.fixed_height_still.url);
+        reactionImage.attr('data-animate', results[i].images.fixed_height.url);
 
         gifDiv.append(p);
         gifDiv.append(reactionImage);
-
         $('#reactions').prepend(gifDiv);
-
       }
     });
-
   });
 };
 
 renderButtons();
 giphyClick();
 
-$('#addReaction').on('click', function(){
-  reactionArray.push(reactionInput.value);
-  renderButtons();
-  giphyClick();
-  return false;
-});
+
 
 };
